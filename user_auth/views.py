8ce496caa27f7878b4d7c495f8d76d50ba339846
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-16 13:34:44 +0300

    add login-user function

--- user_auth/views.py
+++ user_auth/views.py
@@ -3,4 +3,18 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_user(request):
-    return render(request, 'authentication/login.html', {})
+    message='Instagram'
+    if request.method== 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user =authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            messages.success(request, (f"welcome to Instagram"))
+        else:
+            messages.success(request,("Something went wrong"))
+
+            return render(request, 'authentication/login.html')
+    else:
+        return render(request, 'authentication/login.html', {})
