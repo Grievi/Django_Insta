ca0d54bc8ef116609cc25236c6c526dddaf2e290
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-16 14:32:49 +0300

    add a a signup template

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path('login/', views.login_user, name="login"),
-    path('logout', views.logout_user, name='logout'),
+    path('logout/', views.logout_user, name='logout'),
+    path('sign_up/', views.user_signup, name='sign_up'),
 ]
