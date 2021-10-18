6b04d97d51202dd657f292f88f8b0adcdac0cee9
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-18 23:23:44 +0300

    add a setup testcase for class image

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -8,3 +8,13 @@ class TestProfile(TestCase):
         self.user.save()
 
         self.profile_test= Profile(id=1, name="image", profile_photo='default.jpg', bio='Welcome to my Channel', user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+class ImageTest(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='moringa', user=User(username='Grievi'))
+        self.profile_test.save()
+
+        self.image_test = Image(image='default.png', name='test', caption='default test', user=self.profile_test)
