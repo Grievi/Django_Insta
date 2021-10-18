91317f6fd71634c06ae5c01aa79bb9736da4d54e
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-18 23:20:12 +0300

    add a setup test for user instance

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='moringa')
+        self.user.save()
+
+        self.profile_test= Profile(id=1, name="image", profile_photo='default.jpg', bio='Welcome to my Channel', user=self.user)
