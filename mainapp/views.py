a75259dca032edfd145a1c5cdab6d9bc9101d527
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-18 08:11:28 +0300

    fix a login bug

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,11 +4,16 @@ from .models import Image,Profile
 
 @login_required(login_url='login')
 def index(request):
-    
+    message="Welcome to Instagram"
     images = Image.objects.all()
-    return render(request, 'insta/index.html', {'images':images })
+    return render(request, 'insta/index.html', {'images':images, 'message':message })
 
 def profile(request):
     current_user =request.user
+    images = Image.objects.filter(user_id=current_user.id)
     profile = Profile.objects.filter(user_id=current_user.id).first()
-    return render(request, 'profile.html', {})
+    return render(request, 'profile.html', {"images": images, "profile": profile})
+
+def save_image(request):
+    if request.method=='POST':
+       image_name = request.POST['image_name'] 
